536f1fd56f716476bc33be91f0a4d19ecf6cca21
Max <maxskewes@gmail.com>
2020-11-25 17:39:45 -0500

    Creates numbered tiles with JS/JQuery

---
+++ puzzle.js
@@ -0,0 +1,109 @@
+var MARGIN = 2;
+var BORDER = 1;
+
+
+var tileWidth;
+var tileHeight;
+var tiles = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+  [13, 14, 15, null]
+];
+
+var gapX = 3;
+var gapY = 3;
+
+function slideTile(tile, duration) {
+  tile.animate({
+    top: tile.data("y") * tileHeight,
+    left: tile.data("x") * tileWidth
+  }, duration || 200);
+}
+
+function down() {
+  if (gapY > 0) {
+    var tile = tiles[gapY - 1][gapX];
+    tiles[gapY][gapX] = tile;
+    tile.data("y", gapY);
+    slideTile(tile);
+    gapY = gapY - 1;
+    tiles[gapY][gapX] = null;
+  }
+}
+
+function up() {
+  if (gapY < 3) {
+    var tile = tiles[gapY + 1][gapX];
+    tiles[gapY][gapX] = tile;
+    tile.data("y", gapY);
+    slideTile(tile);
+    gapY = gapY + 1;
+    tiles[gapY][gapX] = null;
+  }
+}
+
+function postionTiles() {
+  for (var x = 0; x < 4; x++) {
+    for (var y = 0; y < 4; y++) {
+      var tile = tiles[y][x];
+
+      if (tile) {
+      tile.css({
+        top: tile.data("y") * tileHeight,
+        left: tile.data("x") * tileWidth
+      });
+    }
+    }
+  }
+}
+
+function resize() {
+  var margin = parseInt($("body").css("margin"));
+  var windowWidth = $(window).width() - 2 * margin;
+  var windowHeight = $(window).height() - 2 * margin;
+  
+  tileWidth = Math.floor(windowWidth / 4);
+  tileHeight = Math.floor(windowHeight / 4);
+
+  console.log(tileWidth, tileHeight);
+  var fontSize = Math.min(tileWidth, tileHeight);
+  var extraSpace = 2 * (MARGIN + BORDER);
+
+  $(".tile")
+    .width(tileWidth - extraSpace)
+    .height(tileHeight - extraSpace)
+    .css("fontSize", (0.8 * fontSize) + "px");
+
+  postionTiles();
+}
+
+function initTiles() {
+  var board = $('#board');
+
+  for (y = 0; y < 4; y++) {
+    for (var x = 0; x < 4; x++) {
+      var value = y * 4 + x + 1;
+
+      if (value < 16) {
+        var tile = $('<div class="tile">' + value + '</div>');
+        board.append(tile);
+        tile.data("x", x).data("y", y);
+        tiles[y][x] = tile;
+        if (x % 2) {
+          tile.css("backgroundColor", "Pink");
+        } else {
+          tile.css("backgroundColor", "lightGreen");
+        }
+      }
+    }
+  }
+}
+
+$(
+  function(evt) {
+    $(window).resize(resize);
+    initTiles();
+    resize();
+  }
+);
